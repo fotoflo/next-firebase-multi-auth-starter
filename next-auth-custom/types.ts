c147de772d800069a3911c5ecaa2c60d280df495
Alex Miller <fotoflo@gmail.com>
2022-06-28 18:07:02 +0800

    update the session type

--- next-auth-custom/types.ts
+++ next-auth-custom/types.ts
@@ -3,12 +3,11 @@ import { DefaultSession } from "next-auth";
  * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
  */
 export interface Session {
-  session: {
     user: {
       id: string;
       image: string;
       email: string;
     };
     emailVerified: boolean;
-  } & DefaultSession["session"];
-}
+  
+} & DefaultSession["session"];
