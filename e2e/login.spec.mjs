bf35b6c52046629c4095e12878792cb56471942d
Alex Miller <fotoflo@gmail.com>
2022-06-28 17:13:36 +0800

    add pre-commit hook

--- e2e/login.spec.mjs
+++ e2e/login.spec.mjs
@@ -17,7 +17,7 @@ test("Login", async ({ page }) => {
     .fill(process.env.PLAYWRIGHT_GOOGLE_USERNAME);
   // Click button:has-text("Berikutnya")  works in indonesia...
   await Promise.all([
-    page.waitForNavigation(/*{ url: 'https://accounts.google.com/signin/v2/challenge/pwd?client_id=436476385874-tq0vct74slfd6k8s3qmb5goi9jteh250.apps.googleusercontent.com&scope=openid%20email%20profile&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fgoogle&state=Z1zxDU2Weaf_pkpP_M3ea5i5GJ88naYIrF1Fn_JWIQM&code_challenge=m5aAB0jZuHP4aTsoS93UPLH39Qj2CzvsQcgmU5-ZXbQ&code_challenge_method=S256&flowName=GeneralOAuthFlow&cid=1&navigationDirection=forward&TL=AM3QAYYTpnTuJqiOT6V_k0BnA_vsAkwy0Wqxyyi9MtcRMy70rjQcGRveHShHiE9y' }*/),
+    page.waitForNavigation(/*{ url: 'https://accounts.google.com/signin/v2/challenge/pwd?client_id' }*/),
     page.locator('button:has-text("Berikutnya")').click(),
   ]);
   // Click [aria-label="Masukkan sandi Anda"]
